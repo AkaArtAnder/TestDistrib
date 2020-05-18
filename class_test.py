753d1c67784e7fffde79ebf9dc20189912cf6d86
Andrey <AkaArtAnder@yandex.ru>
2020-05-18 23:01:20 +0300

    Master 1

---
+++ class_test.py
@@ -0,0 +1,18 @@
+"""
+
+ В файле реализован класс для тестирования распределения TestDistrib
+
+"""
+from matplotlib import pyplot as plt
+import seaborn as sb
+import numpy as np
+
+
+class TestDistrib(object):
+
+    """ Класс реализует методы предназначенные для тестирования последовательности на нормальность"""
+    pass
+
+
+if __name__ == "__main__":
+    pass
